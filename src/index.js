dc74f252080442e790fe99bf1402b1090d843890
completejavascript <completejavascript.super@gmail.com>
2019-03-17 22:55:03 +0700

    Connect ReactJS and Redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import App from "./components/App";
 
 const mathReducer = (state = {
   result: 1,
@@ -41,7 +32,7 @@ const mathReducer = (state = {
 };
 
 const userReducer = (state = {
-  name: "Lam",
+  name: "Lam Pham",
   age: 26
 }, action) => {
   switch (action.type) {
@@ -70,7 +61,7 @@ const myLogger = (store) => (next) => (action) => {
 };
 
 const store = createStore(
-  combineReducers({ mathReducer, userReducer }),
+  combineReducers({ math: mathReducer, user: userReducer }),
   applyMiddleware(myLogger, logger)
 );
 
@@ -78,27 +69,9 @@ store.subscribe(() => {
   console.log("State updated:", store.getState());
 });
 
-store.dispatch({
-  type: "ADD",
-  payload: 1
-});
-
-store.dispatch({
-  type: "ADD",
-  payload: 100
-});
-
-store.dispatch({
-  type: "SUBTRACT",
-  payload: 50
-});
-
-store.dispatch({
-  type: "SET_NAME",
-  payload: "Lam Pham"
-});
-
-store.dispatch({
-  type: "SET_AGE",
-  payload: 27
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
